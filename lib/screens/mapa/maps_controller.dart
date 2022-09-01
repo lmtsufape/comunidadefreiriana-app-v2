c422ad606ecabe17e784d43b7caf4a5c285008e8
Caio <caio.vsg@hotmail.com>
2022-09-01 11:02:36 -0300

    trocando versão do sdk

--- lib/screens/mapa/maps_controller.dart
+++ lib/screens/mapa/maps_controller.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: unnecessary_type_check, prefer_typing_uninitialized_variables
 import 'package:flutter/material.dart';
-import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
